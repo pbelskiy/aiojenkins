142800903d5e5e66861d2b724ff315f12efee026
Petr Belskiy <petr.belskiy@gmail.com>
2020-04-25 00:38:36 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='aiojenkins',
-    version='0.1.1',
+    version='0.1.2',
     description='Asynchronous library of Jenkins API endpoints based on aiohttp',
     long_description_content_type='text/markdown',
     long_description=README,
