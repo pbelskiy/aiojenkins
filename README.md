d84ef41f0bc666426c36b14db1f1fafc6d9fb98c
Petr Belskiy <petr.belskiy@gmail.com>
2020-08-02 13:21:00 +0300

    add python versions badge

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ![Tests](https://github.com/pbelskiy/aiojenkins/workflows/Tests/badge.svg)
 ![Coveralls github](https://img.shields.io/coveralls/github/pbelskiy/aiojenkins?label=Coverage)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/aiojenkins?label=Python)
 ![PyPI - Downloads](https://img.shields.io/pypi/dm/aiojenkins?color=1&label=Downloads)
 
 Asynchronous python library of Jenkins API endpoints based on aiohttp 🥳
--- setup.py
+++ setup.py
@@ -5,10 +5,17 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='aiojenkins',
-    version='0.4.2',
+    version='0.4.3',
     description='Asynchronous library of Jenkins API based on aiohttp',
     long_description_content_type='text/markdown',
     long_description=README,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
     license='MIT',
     packages=find_packages(),
     author='Petr Belskiy',
@@ -19,7 +26,7 @@ setup_args = dict(
 )
 
 install_requires = [
-    'aiohttp'
+    'aiohttp<4.0'
 ]
 
 if __name__ == '__main__':
