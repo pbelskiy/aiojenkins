c6decb64cc18d69d75c8376b6cc6361ea1ae4b45
Petr Belskiy <petr.belskiy@gmail.com>
2020-09-10 01:20:04 +0300

    add views:is_exists

--- tests/test_views.py
+++ tests/test_views.py
@@ -8,3 +8,9 @@ async def test_get_views():
     views = await jenkins.views.get_all()
     assert len(views) == 1
     assert 'all' in map(lambda s: s.lower(), views)
+
+
+@pytest.mark.asyncio
+async def test_is_exists():
+    assert (await jenkins.views.is_exists('All') or  # <= 1.554
+            await jenkins.views.is_exists('all')) is True
