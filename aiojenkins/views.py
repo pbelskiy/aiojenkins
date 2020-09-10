ce2b7756a31ff7ac324fa1c624b8da1685152879
Petr Belskiy <petr.belskiy@gmail.com>
2020-09-11 00:04:31 +0300

    add views:is_exists (#11)
    * add views:is_exists
    
    * fix flake8

--- aiojenkins/views.py
+++ aiojenkins/views.py
@@ -6,3 +6,7 @@ class Views:
     async def get_all(self) -> dict:
         status = await self.jenkins.get_status()
         return {v['name']: v for v in status['views']}
+
+    async def is_exists(self, name: str) -> bool:
+        views = await self.get_all()
+        return name in views
--- tests/test_views.py
+++ tests/test_views.py
@@ -8,3 +8,13 @@ async def test_get_views():
     views = await jenkins.views.get_all()
     assert len(views) == 1
     assert 'all' in map(lambda s: s.lower(), views)
+
+
+@pytest.mark.asyncio
+async def test_is_exists():
+    version = await jenkins.get_version()
+
+    if version.major < 2:
+        assert await jenkins.views.is_exists('All') is True
+    else:
+        assert await jenkins.views.is_exists('all') is True
