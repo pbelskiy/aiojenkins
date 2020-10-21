cc947b888338fb95567e3992fc49816a47059f0e
Petr Belskiy <petr.belskiy@gmail.com>
2020-10-22 00:00:09 +0300

    add Python 3.9 to CI

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     license='MIT',
     packages=['aiojenkins'],
