daec36504a909c91dab26fc5487427e036b92861
Petr Belskiy <petr.belskiy@gmail.com>
2022-06-02 00:39:47 +0300

    bump version to 0.7.5

--- aiojenkins/__init__.py
+++ aiojenkins/__init__.py
@@ -1,6 +1,6 @@
 from .exceptions import JenkinsError, JenkinsNotFoundError
 from .jenkins import Jenkins
 
-__version__ = '0.7.4'
+__version__ = '0.7.5'
 
 __all__ = ('Jenkins', 'JenkinsError', 'JenkinsNotFoundError')
