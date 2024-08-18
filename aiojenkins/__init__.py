795600560a1bd036a9565d77784cfb2ed68c9618
Petr Belskiy <petr.belskiy@gmail.com>
2024-08-18 13:49:41 +0300

    bump to 0.8.0

--- aiojenkins/__init__.py
+++ aiojenkins/__init__.py
@@ -1,6 +1,6 @@
 from .exceptions import JenkinsError, JenkinsNotFoundError
 from .jenkins import Jenkins
 
-__version__ = '0.7.8'
+__version__ = '0.8.0'
 
 __all__ = ('Jenkins', 'JenkinsError', 'JenkinsNotFoundError')
